bc981227be3cf50e2078cf67a31202fbaf1bb6d7
jonhnny <jonathan99moura@hotmail.com>
2017-05-10 11:21:32 -0200

    arquivo xml

--- solucao1.js
+++ solucao1.js
@@ -1,3 +1,4 @@
+//baixar todos os pacotes presentes no https://tiipos.github.io/2017.js.arquivos_texto.html
 
 var arquivo = process.argv[2];
 var separador = arquivo.split(".");
@@ -17,7 +18,6 @@ else{
 	var txto = json;
 	//pegar o 1 valor
 	txto.intervalos[0]; //100
-
 	//var separador = txto.split(",");
 
 	//Para pegar a cadeia inteira, você pode simplesmente concatenar tudo usando join com seu concatenador, já que se trata de um array:
@@ -39,12 +39,46 @@ else{
  		}
 	}
 	else if(extensão.toString() == "xml"){
-		console.log("arquivo xml");
+	    
+		//ler arquivo xml
+		lerXMLComXml2Js(txt, function(err, result) {
+			
+			console.log(result.intervalos.inter[0].valores[0]);
+			console.log("------------");
+			var variaveis = result.intervalos.inter[0].valores[0];
+			txto.intervalos[0];
+			txto.intervalos.join(",");
+		});
+		
+		//var txto = variaveis;
+		
+		//txto.intervalos[0]; //100
+		
+
+		//Para pegar a cadeia inteira, você pode simplesmente concatenar tudo usando join com seu concatenador, já que se trata de um array:
+		/*txto.intervalos.join(",");
+
+		for (var i in txto.intervalos) {
+
+   		if(txto.intervalos[i-1] == txto.intervalos[i]-1){
+		console.log(txto.intervalos[i]); //envia pro console os valores
+ 		}
+ 		else{
+ 			if(txto.intervalos[i-1] != txto.intervalos[i]-1){
+ 				console.log("-------------- ");
+ 				console.log(txto.intervalos[i]);
+ 			}
+ 			else{console.log(txto.intervalos[i]);}
+ 			//console.log("-------------- ");
+ 			}
+ 		}*/
 	}
+	
 	else if(extensão.toString() == "csv"){
 		console.log("arquivo csv");
-	}
+	}		
 }
+
 	/*
 	vai ficar assim
 	100, 101, 102, 103, 104, 105, (ok)
@@ -60,4 +94,13 @@ function lerJSONComRequire(txt) {
     var json = require(txt);
     // console.log(json);
     return json;
+}
+
+function lerXMLComXml2Js(arquivo, funcao) {
+    var fs = require('fs');
+    var xmlParser = require('xml2js').parseString;
+
+    fs.readFile(arquivo, function(err, data) {
+        xmlParser(data, funcao);
+    });
 }
